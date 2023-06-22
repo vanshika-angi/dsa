b14bf7625be89d03299f80132414af494062fe9c
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-06-22 18:38:47 +0530

    Added solution - LeetHub

---
+++ Longest Consecutive 1's - GFG/longest-consecutive-1s.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution{
+    
+    /*  Function to calculate the longest consecutive ones
+    *   N: given input to calculate the longest consecutive ones
+    */
+    public static int maxConsecutiveOnes(int N) {
+        
+        // Your code here
+        int ans=0,count=0;
+        while(N!=0)
+        {
+            if((N&1) != 0)
+            {
+                count++;
+                if(count>ans)ans=count;
+            }
+            else
+            {
+                count=0;
+            }
+            N=N>>1;
+        }
+        return ans;
+        
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class Main {
+    
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//input n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxConsecutiveOnes() function
+		    System.out.println(obj.maxConsecutiveOnes(n));
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
