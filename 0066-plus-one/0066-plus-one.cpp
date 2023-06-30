1050ca7af5a3efbc709a2e0b9b615f5cd09aef9a
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-07-01 00:03:31 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (75.78%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=1;
+       for(int i=digits.size()-1;i>=0;i--)
+       {
+          int sum=digits[i]+carry;
+        //   cout<<sum<<endl;
+           digits[i]=sum%10;
+           carry=sum/10;
+       }
+        if(carry!=0)
+        {
+            digits.insert(digits.begin(),carry);
+        }
+        return digits;
+    }
+};
