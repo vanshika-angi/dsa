d42278fdd34d88251ae0fdc1ccc03811885075c2
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-07-13 14:09:46 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,23 +10,45 @@ using namespace std;
 
 class Solution{
   public:
-    int helper(int ind,int len,int price[],vector<vector<int>>&dp)
-    {
-        if(ind==0)
-        {
-            return  len*price[0];
-        }
-        if(dp[ind][len]!=-1)return dp[ind][len];
-        int notcut= 0 +helper(ind-1,len,price,dp);
-        int cut=0;
-        int rod=ind+1;
-        if(len-rod>=0)cut = price[ind] + helper(ind,len-rod,price,dp);
+    // int helper_memo(int ind,int len,int price[],vector<vector<int>>&dp)
+    // {
+    //     if(ind==0)
+    //     {
+    //         return  len*price[0];
+    //     }
+    //     if(dp[ind][len]!=-1)return dp[ind][len];
+    //     int notcut= 0 +helper(ind-1,len,price,dp);
+    //     int cut=0;
+    //     int rod=ind+1;
+    //     if(len-rod>=0)cut = price[ind] + helper(ind,len-rod,price,dp);
         
-        return dp[ind][len] = max(notcut,cut);
-    }
+    //     return dp[ind][len] = max(notcut,cut);
+    // }
+    
+    
     int cutRod(int price[], int n) {
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return helper(n-1,n,price,dp);
+        vector<vector<int>>dp(n,vector<int>(n+1,0));
+        // return helper_tab(n-1,n,price,dp);
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[0][i]=i*price[0];
+        }
+        
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int len=0;len<=n;len++)
+            {
+                int notcut= dp[ind-1][len];
+                int cut=0;
+                int rod=ind+1;
+                if(len-rod>=0)cut = price[ind] + dp[ind][len-rod];
+                
+                dp[ind][len] = max(notcut,cut);
+            }
+        }
+        
+        return dp[n-1][n];
     }
 };
 
