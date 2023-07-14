1fea0f61d5cab3d1a1d4f7550dfb5ebb82744df2
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-07-14 19:52:44 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -10,45 +10,87 @@ using namespace std;
 
 class Solution {
   public:
-   bool dfs(int node, vector<int> adj[], vector<int>&vis, vector<int>&pathvis,vector<int>&check)
-    {
-        vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
-        for(auto it:adj[node])
-        {
-            if(!vis[it])
-            {
-                if(dfs(it,adj,vis,pathvis,check)==true)return true;
-            }
-            else if(pathvis[it])
-            {
-                return true;
-            }
-        }
-        check[node]=1;
-        pathvis[node]=0;
-        return false;
-    }
+//   bool dfs(int node, vector<int> adj[], vector<int>&vis, vector<int>&pathvis,vector<int>&check)
+//     {
+//         vis[node]=1;
+//         pathvis[node]=1;
+//         check[node]=0;
+//         for(auto it:adj[node])
+//         {
+//             if(!vis[it])
+//             {
+//                 if(dfs(it,adj,vis,pathvis,check)==true)return true;
+//             }
+//             else if(pathvis[it])
+//             {
+//                 return true;
+//             }
+//         }
+//         check[node]=1;
+//         pathvis[node]=0;
+//         return false;
+//     }
     
     
-    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
-        vector<int>vis(V,0);
-        vector<int>pathvis(V,0);
+//     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+//         vector<int>vis(V,0);
+//         vector<int>pathvis(V,0);
+//         vector<int>safe;
+//         vector<int>check(V,0);
+//         for(int i=0;i<V;i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 dfs(i,adj,vis,pathvis,check);
+//             }
+//         }
+//         for(int i=0;i<V;i++)
+//         {
+//             if(check[i]==1)safe.push_back(i);
+//         }
+        
+//         return safe;
+//     }
+
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]){
+        
+        vector<int>adjR[V];
+        vector<int>indegree(V,0);
+        queue<int>q;
         vector<int>safe;
-        vector<int>check(V,0);
+        
         for(int i=0;i<V;i++)
         {
-            if(!vis[i])
+            for(auto it:adj[i])
             {
-                dfs(i,adj,vis,pathvis,check);
+                adjR[it].push_back(i);
+                indegree[i]++;
+                
             }
-        }
-        for(int i=0;i<V;i++)
-        {
-            if(check[i]==1)safe.push_back(i);
         }
         
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            safe.push_back(node);
+            
+            for(auto it:adjR[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        
+        sort(safe.begin(),safe.end());
         return safe;
     }
 };
