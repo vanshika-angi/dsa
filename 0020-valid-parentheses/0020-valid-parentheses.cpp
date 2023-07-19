471a3fe729315db711e796918904c8a1ccdef29b
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-07-19 22:50:49 +0530

    Time: 4 ms (40.41%), Space: 6.4 MB (8.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,21 +6,11 @@ public:
         for(int i=1;i<s.length();i++)
         {
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')st.push(s[i]);
-            else if(s[i]==')')
-            {
-                if(!st.empty() && st.top()=='(')st.pop();
-                else return false;
-            }
-            else if(!st.empty() &&  s[i]=='}')
-            {
-                if(st.top()=='{')st.pop();
-                else return false;
-            }
-            else if(!st.empty() && s[i]==']')
-            {
-                if(st.top()=='[')st.pop();
-                else return false;
-            }
+            
+            else if(!st.empty()  &&  ((s[i]=='}' && st.top()=='{') || (s[i]==']' && st.top()=='[') || (s[i]==')' && st.top()=='(')))st.pop();
+            else return false;
+            
+            
         }
         return st.empty();
     }
