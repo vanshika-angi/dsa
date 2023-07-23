cd68b6587e55a2a51b21f993cede0393aad46714
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-07-23 19:11:22 +0530

    Added solution - LeetHub

---
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            st.push(i);
+            for(int j=0;j<n;j++)
+            {
+               if(M[i][j]==1)
+               {
+                   st.pop();
+                   break;
+               }
+            }
+        }
+        int ans=-1;
+        while(!st.empty())
+        {
+            int element=st.top();
+            st.pop();
+            int flag=1;
+            for(int i=0;i<n;i++)
+            {
+                if(M[i][element]!=1 && i!=element)flag=0;
+            }
+            
+            if(flag==1)ans=element;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
