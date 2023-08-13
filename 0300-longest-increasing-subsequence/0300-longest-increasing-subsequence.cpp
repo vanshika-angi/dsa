450d597f7d216a24b90959cea0ba97bdd65cc795
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-08-13 23:57:09 +0530

    Time: 810 ms (29.96%), Space: 292.3 MB (8.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int helper(int ind,int lar_ind,vector<int>& nums,vector<vector<int>>&dp)
+    int f(int ind,int lar_ind,vector<int>&nums,vector<vector<int>>&dp)
     {
         if(ind<0)return 0;
         if(dp[ind][lar_ind+1]!=-1)return dp[ind][lar_ind+1];
-        int notpick = 0 + helper(ind-1,lar_ind,nums,dp);
-        int pick=INT_MIN;
-        if(lar_ind==-1 || nums[ind]<nums[lar_ind])
-            pick = 1+helper(ind-1,ind,nums,dp);
+        int npick = 0+ f(ind-1,lar_ind,nums,dp);
+        int pick =INT_MIN;
+        if(lar_ind==-1 || nums[lar_ind]>nums[ind])
+        {
+           pick = 1 + f(ind-1,ind,nums,dp); 
+        }
         
-        return dp[ind][lar_ind+1] = max(notpick,pick);
+        return dp[ind][lar_ind+1] = max(npick,pick);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-//         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-//         for(int i=1;i<=n;i++)
-//         {
-//             for(int j=i-1;j>=-1;j--)
-//             {
-//                 int notpick = 0 + dp[i-1][j+1];
-//                 int pick=INT_MIN;
-//                 if( j==-1 || nums[i-1]<nums[j-1])
-//                     pick = 1+dp[i-1][i-1];
-                
-//                 dp[i][j] = max(notpick,pick);
-//             }
-//         }
-//         return dp[n-1][n-1];
-       return helper(n-1,-1,nums,dp); 
+       return f(n-1,-1,nums,dp); 
     }
 };
