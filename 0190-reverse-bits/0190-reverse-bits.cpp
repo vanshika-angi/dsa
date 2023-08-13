8b6aa92b9ae026e390813c6b380e833de07b29e7
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-08-13 14:01:21 +0530

    Time: 4 ms (35.00%), Space: 6 MB (60.35%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,12 +4,11 @@ public:
         int res=0;
         for(int i=0;i<32;i++)
         {
-            int lsb=n&1;
-            int mask= lsb <<(31-i);
-            res=res|mask;
+            int temp=n&1;
+            int mask = temp<<(31-i);
+            res = res|mask;
             n=n>>1;
         }
-
         return res;
     }
 };
