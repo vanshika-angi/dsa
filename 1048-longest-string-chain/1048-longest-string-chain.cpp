33279b57a9c87e5ed9e4ad1db956281f54d7348a
vanshika-angi <50419222+vanshika-angi@users.noreply.github.com>
2023-10-22 12:47:36 +0530

    Time: 1791 ms (8.67%), Space: 635.4 MB (5.62%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,38 +1,34 @@
-// bool comp(string &s1,string &s2)
-//     {
-//         return s1.size()<s2.size();
-//     }
-
-bool comp(string& s1, string& s2){
-    return s1.size() < s2.size();
+bool comp(string&s1, string&s2)
+{
+    return s1.size()<s2.size();
 }
-
-
 class Solution {
 public:
-    
-    
-    bool check(int x,int y,vector<string>& words)
+    int helper(int i,int j,vector<string>& words)
     {
-        string s1=words[x];
-        string s2=words[y];
-        if(s1.size() != s2.size() + 1) return false;
-    
-        int first = 0;
-        int second = 0;
-
-        while(first < s1.size()){
-            if(second < s2.size() && s1[first] == s2[second]){
-                first ++;
-                second ++;
+        string word1=words[i];
+        string word2=words[j];
+        
+        if(word1.size()!=word2.size() + 1)return false;
+        int first=0,second=0;
+        
+        while(first<word1.size())
+        {
+            if(second<word2.size() && word1[first]==word2[second])
+            {
+                first++;
+                second++;
             }
-            else first ++;
+            else
+            {
+                first++;
+            }
         }
-        if(first == s1.size() && second == s2.size()) return true;
-        else return false;
+        
+        if(first==word1.size() && second==word2.size())return true;
+        return false;
     }
     int longestStrChain(vector<string>& words) {
-
         int n=words.size();
         vector<int>dp(n,1);
         int maxi=1;
@@ -41,64 +37,19 @@ public:
         {
             for(int j=0;j<i;j++)
             {
-                if(check(i,j,words) && dp[i]<dp[j]+1)
+                if(helper(i,j,words) && dp[i]<dp[j]+1)
                 {
                     dp[i]=dp[j]+1;
                 }
-                
-                if(dp[i] > maxi)
-                maxi = dp[i];
             }
             
+            if(dp[i]>maxi)
+            {
+                maxi=dp[i];
+            }
         }
         
         return maxi;
+        
     }
-    
-    
-//     bool compare(string& s1, string& s2){
-//     if(s1.size() != s2.size() + 1) return false;
-    
-//     int first = 0;
-//     int second = 0;
-    
-//     while(first < s1.size()){
-//         if(second < s2.size() && s1[first] == s2[second]){
-//             first ++;
-//             second ++;
-//         }
-//         else first ++;
-//     }
-//     if(first == s1.size() && second == s2.size()) return true;
-//     else return false; 
-// }
-
-
-
-// int longestStrChain(vector<string>& arr){
-
-//     int n = arr.size();
-    
-//     //sort the array
-    
-//     sort(arr.begin(), arr.end(),comp);
-
-//     vector<int> dp(n,1);
-    
-//     int maxi = 1;
-    
-//     for(int i=0; i<=n-1; i++){
-        
-//         for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-            
-//             if( compare(arr[i], arr[prev_index]) && 1 + dp[prev_index] > dp[i]){
-//                 dp[i] = 1 + dp[prev_index];
-//             }
-//         }
-        
-//         if(dp[i] > maxi)
-//             maxi = dp[i];
-//     }
-//     return maxi;
-// }
 };
